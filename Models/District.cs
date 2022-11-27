b82aa676d0dc5f3d91af566fc0ae495fa61de847
Krzysztof Kopij <115408905+Mumekera@users.noreply.github.com>
2022-11-27 21:08:03 +0100

    Add files via upload

---
+++ Models/District.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    public class District
+    {
+
+
+        public int Id { get; set; }
+        public List<string> Name { get; set; } = new List<string> { "Retkinia", "Łódź Kaliska", "Śródmieście", "Widzew", "Janów" };
+        public int Distance { get; set; }
+
+    }
+}
