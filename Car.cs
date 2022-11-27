ec6e25b3568ae9ab083976b4174f63442053e693
Krzysztof Kopij <115408905+Mumekera@users.noreply.github.com>
2022-11-27 19:37:32 +0100

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    public class Car
+    {
+
+        public int Id { get; set; }
+        public List<string> Name { get; set; } = new List<string>() { "Ford mondeo", "Dacia Logan", "Toyota Avensis", "Mercedes E220", "Hyundai Lantra" };
+        public bool Status { get; set; }
+        public List<District> DistrictName { get; set; } = new List<District>();
+
+    }
+}
